767472e0bb1f102d2d900455637d5ca596dbedc1
Colin Stevens <colinstevens06@gmail.com>
2020-01-05 17:21:46 -0500

    cleaned up some bugs

--- app.js
+++ app.js
@@ -2,33 +2,42 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 function managerPrompts() {
-   inquirer.prompt([
-      {
-         type: "input",
-         message: "Manager name: "
-         name: "managerName"
-      },
-      {
-         type: "input",
-         message: "What is your employee ID?"
-         name: "managerID",
-      },
-      {
-         type: "input",
-         message: "What is your email address?",
-         name: "managerEmail"
-      },
-      {
-         type: "input",
-         message: "What is your office number?",
-         name: "managerOffice"
-      }
-   ]);
+   inquirer
+      .prompt([
+         {
+            type: "input",
+            message: "Manager name: ",
+            name: "managerName"
+         },
+         {
+            type: "input",
+            message: "What is your employee ID?",
+            name: "managerID"
+         },
+         {
+            type: "input",
+            message: "What is your email address?",
+            name: "managerEmail"
+         },
+         {
+            type: "input",
+            message: "What is your office number?",
+            name: "managerOffice"
+         }
+      ])
+      .then(function(response) {
+         let managerName = response.managerName;
+         let managerID = response.managerID;
+         let managerEmail = response.managerEmail;
+         let managerOffice = response.managerOffice;
+
+         console.log(managerName);
+      });
 }
 
 function employeePrompts() {
-   inquirer.prompt(
-      [
+   inquirer
+      .prompt([
          {
             type: "list",
             message: "What is the employee's role?",
@@ -45,32 +54,30 @@ function employeePrompts() {
             message: "What is the employee's email address?",
             name: "employeeEmail"
          }
-      ]
-   ).then(function(response) {
-      let employeeType = response.employeeType;
-      
-      if (employeeType === "Engineer") {
-         inquirer.prompt(
-            [
+      ])
+      .then(function(response) {
+         let employeeType = response.employeeType;
+         let employeeID = response.employeeID;
+         let employeeEmail = response.employeeEmail;
+
+         if (employeeType === "Engineer") {
+            inquirer.prompt([
                {
                   type: "input",
                   message: "What is your employee's GitHub username?",
                   name: "gitHubUN"
                }
-            ]
-         )
-      } else {
-         inquirer.prompt(
-            [
+            ]);
+         } else {
+            inquirer.prompt([
                {
                   type: "input",
-                  message: "Where does the intern go to school?"
+                  message: "Where does the intern go to school?",
                   name: "internSchool"
                }
-            ]
-         )
-      }
-   })
+            ]);
+         }
+      });
 }
 
 // need to ask the manager for their information first
