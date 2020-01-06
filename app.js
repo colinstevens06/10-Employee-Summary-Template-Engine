27ef0111a0fb51988dfdc0cdb09c386c556e0806
Colin Stevens <colinstevens06@gmail.com>
2020-01-06 12:39:30 -0500

    now generating an HTML page with a correct roster

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const cards = require("./templates/Cards");
 
 let employeeID = 1;
 let employeeList = [];
@@ -38,10 +37,9 @@ function managerPrompts() {
             managerOffice
          );
 
-         console.log(manager.createCard());
-         employeeID++;
-
          employeeList.push(manager);
+
+         employeeID++;
 
          console.log(`
          ~~~~~~~~~~~~~~
@@ -111,6 +109,7 @@ function employeePrompts() {
                   if (response.moreEmployees === "Yes") {
                      employeePrompts();
                   } else {
+                     generatePage();
                      return;
                   }
                });
@@ -138,18 +137,79 @@ function employeePrompts() {
                      employeeEmail,
                      employeeSchool
                   );
+
                   employeeList.push(intern);
+
                   employeeID++;
 
                   if (response.moreEmployees === "Yes") {
                      employeePrompts();
                   } else {
+                     generatePage();
                      return;
                   }
                });
          }
       });
-   console.log(employeeList);
+   // console.log(employeeList);
+}
+
+function generatePage() {
+   let allCards = "";
+
+   employeeList.forEach(item => {
+      let cardString = item.createCard();
+      allCards += cardString;
+   });
+
+   let fullHTML = `
+   <!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+      <link
+         rel="stylesheet"
+         href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+         crossorigin="anonymous"
+      />
+
+      <script
+         src="https://kit.fontawesome.com/ab3fd93a87.js"
+         crossorigin="anonymous"
+      ></script>
+
+      <title>My Team Roster</title>
+   </head>
+   <body>
+      <div
+         class="container-fluid bg-danger text-center d-flex align-items-center justify-content-center"
+         style="height: 20vh"
+      >
+         <div class="h1 text-white" style="display: inline-block;">
+            My Team
+         </div>
+      </div>
+
+      <div class="container mt-5">
+         <!-- start of card group -->
+         <div class="card-deck d-inline-flex justify-content-center">
+            ${allCards}
+         </div>
+         <!-- end of card group -->
+      </div>
+   </body>
+</html>
+   `;
+
+   fs.writeFile("./output/roster.html", fullHTML, function(err) {
+      if (err) {
+         return console.log(err);
+      }
+   });
 }
 
 managerPrompts();
